6d66135eef99af5d1547ef103df2a4111ad73ea4
murtazasmart <murtazasmart@hotmail.com>
2017-11-24 23:50:04 +0530

    tried firevase

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -1,6 +1,34 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
+const firebase = require("firebase-admin");
+// const functions = require('firebase-functions');
+var config = {
+    apiKey: "AIzaSyDYLBJK083sPEc6RjqPp99eO4BDCAhbeA0",
+    authDomain: "fir-db-6a0b7.firebaseapp.com",
+    databaseURL: "https://fir-db-6a0b7.firebaseio.com",
+    storageBucket: "fir-db-6a0b7.appspot.com"
+};
+firebase.initializeApp(config);
+var defaultDatabase = firebase.database();
+defaultDatabase.ref('users/' + "123").set({
+    username: "mujja"
+});
+// firebase.initializeApp({
+//   credential: firebase.credential.cert(config)
+// });
+// var db = firebase.firestore();
+// var docRef = db.collection('slack-instapaper-middleware').doc('trial');
+// var setAda = docRef.set({
+//     first: 'Ada',
+//     last: 'Lovelace',
+//     born: 1815
+// });
+// setAda.then((data) => {
+//   console.log(data);
+// }).catch((err) => {
+//   console.log(err);
+// })
 const router = express_1.Router();
 exports.router = router;
 router.get("/", (req, res, next) => {
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,50 @@
 import { NextFunction, Request, Response, Router } from "express";
+import * as firebase from "firebase-admin";
+// const functions = require('firebase-functions');
+
+// var config = {
+//   apiKey: "AIzaSyDYLBJK083sPEc6RjqPp99eO4BDCAhbeA0",
+//   authDomain: "fir-db-6a0b7.firebaseapp.com",
+//   databaseURL: "https://fir-db-6a0b7.firebaseio.com",
+//   projectId: "fir-db-6a0b7",
+//   storageBucket: "fir-db-6a0b7.appspot.com",
+//   messagingSenderId: "254583806525"
+// };
+
+// firebase.initializeApp({
+//   credential: firebase.credential.cert({
+//     projectId: config.apiKey,
+//     clientEmail: config.authDomain,
+//     privateKey: "-----BEGIN PRIVATE KEY-----\n<MY_PRIVATE_KEY>\n-----END PRIVATE KEY-----\n"
+//   }),
+// databaseURL: config.databaseURL
+// });
+
+// var defaultDatabase = firebase.database();
+
+// defaultDatabase.ref('users/' + "123").set({
+//   username: "mujja"
+// });
+
+// firebase.initializeApp({
+//   credential: firebase.credential.cert(config)
+// });
+
+// var db = firebase.firestore();
+
+// var docRef = db.collection('slack-instapaper-middleware').doc('trial');
+
+// var setAda = docRef.set({
+//     first: 'Ada',
+//     last: 'Lovelace',
+//     born: 1815
+// });
+
+// setAda.then((data) => {
+//   console.log(data);
+// }).catch((err) => {
+//   console.log(err);
+// })
 
 const router = Router();
 
@@ -10,7 +56,7 @@ router.get("/", (req: Request, res: Response, next: NextFunction) => {
 });
 
 router.post("/", (req, res) => {
-  console.log(req.body.challenge);
+  console.log(req.body);
   res.status(200);
   res.type('application/json');
   res.json({

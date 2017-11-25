82f2ddf1c9f03436877028300b27c53ffc7fb55c
murtazasmart <murtazasmart@hotmail.com>
2017-11-26 00:53:36 +0530

    m

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -17,32 +17,32 @@ router.get("/", (req: Request, res: Response, next: NextFunction) => {
 router.post("/", (req, res) => {
   // console.log(req.body);
   console.log(config.nsbm);
-  // const options = {
-  //   url: 'https://slack.com/api/channels.history',
-  //   method: 'POST',
-  //   form: {
-  //     "token": config.apiToken,
-  //     "channel": config.channels.articles,
+  const options = {
+    url: 'https://slack.com/api/channels.history',
+    method: 'POST',
+    form: {
+      "token": config.apiToken,
+      "channel": config.channels.articles,
 
-  //   }
-  // };
-  superagent.post("https://slack.com/api/channels.history").send({
-    "token": "xoxp-164141768337-164926927172-277378214515-7d70e0ba2b3880c28de368a583063a49",
-    "channel": "C832626CU",
-    "ts": "latest",
-    "inclusive": "true",
-    "count": 1
-  }).end((err: any, result: any) => {
-    console.log("err "+err);
-    console.log("res "+result);
-    res.json({
-      "challenge": "hello",
-      result
-    });
-  })
-  // request(options, (response) => {
-    
-  // }); 
+    }
+  };
+  // superagent.post("https://slack.com/api/channels.history").send({
+  //   "token": "xoxp-164141768337-164926927172-277378214515-7d70e0ba2b3880c28de368a583063a49",
+  //   "channel": "C832626CU",
+  //   "ts": "latest",
+  //   "inclusive": "true",
+  //   "count": 1
+  // }).end((err: any, result: any) => {
+  //   console.log("err "+err);
+  //   console.log("res "+result);
+  //   res.json({
+  //     "challenge": "hello",
+  //     result
+  //   });
+  // })
+  request(options, (response) => {
+    res.json(response);
+  }); 
   // if(validUrl.isUri(req.body.event.message.text)){
   //   console.log("URL is "+ req.body.event.message.text);
   // }

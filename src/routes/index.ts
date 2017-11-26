48f245a4b6b4cdb9b9862eb75c1f9569ccd09841
murtazasmart <murtazasmart@hotmail.com>
2017-11-26 21:56:30 +0530

    m

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -29,12 +29,12 @@ router.post("/", (req, res) => {
   //   }
   // };
   superagent.post("https://slack.com/api/channels.history").send({
-    "token": "xoxp-164141768337-164926927172-277378214515-7d70e0ba2b3880c28de368a583063a49",
+    "token": "xoxp-164141768337-164926927172-277595731282-7c4b924abf061947ddd7bf07346b4a57",
     "channel": "C832626CU",
     "ts": "latest",
     "inclusive": "true",
     "count": 1
-  }).set("Content-Type", "application/json").end((err: any, result: any) => {
+  }).set("Content-Type", "").end((err: any, result: any) => {
     console.log("err " + err);
     console.log("res " + result);
     res.json({
